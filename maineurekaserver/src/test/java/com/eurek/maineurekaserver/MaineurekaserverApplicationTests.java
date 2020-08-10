6fc1e294eef11134aac6fc94be0bf9322e7ce22d
pavan kalyan <immadipavankalyan5@gmail.com>
2020-08-09 22:36:04 -0700

    main eureka server commmiting here
    Signed-off-by: pavan kalyan<immadipavankalyan5@gmail.com>

---
+++ maineurekaserver/src/test/java/com/eurek/maineurekaserver/MaineurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurek.maineurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaineurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
